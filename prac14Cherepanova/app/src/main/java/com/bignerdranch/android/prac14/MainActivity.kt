6feaf8ff80dad9c29cf34c1a0307b270ba69653f
NastyaCherepanova <123734001+NastyaCherepanova@users.noreply.github.com>
2023-04-07 15:45:52 +0500

    Add files via upload

---
+++ prac14Cherepanova/app/src/main/java/com/bignerdranch/android/prac14/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.bignerdranch.android.prac14
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var questionTextView: TextView
+    private lateinit var nextButton: Button
+    private lateinit var backButton: Button
+    private var currentIndex =0
+
+    private val questionBank = listOf(
+
+    )
+
+}
